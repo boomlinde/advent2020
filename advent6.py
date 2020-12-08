146942e9d68382fb7f09d33c429a89109396288e
Philip Linde <linde.philip@gmail.com>
2020-12-08 16:28:05 +0100

    Solution 6

---
+++ advent6.py
@@ -0,0 +1,21 @@
+import sys
+
+def mask(char):
+    return 1 << (ord(char) - ord('a'))
+
+def nbits(n):
+    s = 0
+    while n:
+        s += n & 1
+        n >>= 1
+    return s
+
+groups = [0]
+for line in sys.stdin:
+    if line == '\n':
+        groups.append(0)
+        continue
+    groups[-1] |= sum(mask(q) for q in line.strip())
+
+
+print sum(nbits(g) for g in groups)
