196437edf22c8f0c23e43a621209f3862bad8d14
Philip Linde <linde.philip@gmail.com>
2020-12-10 11:25:22 +0100

    Simplify solution 7
    Reverse the relationship in the rules dictionary and remove topological
    sorting

--- advent7.py
+++ advent7.py
@@ -5,49 +5,25 @@ def parse(line):
     m = re.match(r'^(.+) bags contain (.+)$', line)
     name = m.group(1)
 
-    content = {}
+    out = []
     for item in  m.group(2).split(', '):
         if item == 'no other bags.':
             continue
         m = re.match(r'^(\d+) ([a-z ]+) bags?.?', item)
-        content[m.group(2)] = int(m.group(1))
-
-    return (name, content)
-
-def peek(s):
-    item = s.pop()
-    s.add(item)
-    return item
-
-def visit(graph, node, unmarked, tempmarks, out):
-    if node not in unmarked:
-        return
-    if node in tempmarks:
-        raise ValueError("cycle")
-
-    tempmarks.add(node)
-    for child in graph[node].keys():
-        visit(graph, child, unmarked, tempmarks, out)
-    tempmarks.remove(node)
-
-    unmarked.remove(node)
-    out.append((node, graph[node]))
-
-def toposort(graph):
-    unmarked = set(graph.keys())
-    out = []
-    while unmarked:
-        visit(graph, peek(unmarked), unmarked, set(), out)
+        out.append((m.group(2), name))
     return out
+
+def find(color, rules, found=None):
+    if found is None:
+        found = set()
+    for parent in rules.get(color, ()):
+        found.add(parent)
+        find(parent, rules, found)
+    return found
 
 rules = {}
 for line in sys.stdin:
-    rules.update(dict([parse(line.strip())]))
+    for contained, container in parse(line.strip()):
+        rules[contained] = rules.get(contained, ()) + (container,)
 
-bags = set()
-for bag, content in toposort(rules):
-    for other in content.keys():
-        if other in bags or other == 'shiny gold':
-            bags.add(bag)
-
-print len(bags)
+print len(find('shiny gold', rules))
